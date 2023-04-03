f03d1bf977173a8076a0f5c1992085ccfd7b64cf
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-04-02 20:47:20 -0400

    done with initial page and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,4 @@
-$('.card-title').text('Project');
-$('p.card-text').text('This is a project');
+const openWeatherApiKey = 'f06064b77c654c7cbd74422f1aaaf3c6';
+
+// $('.card-title').text('Project');
+// $('p.card-text').text('This is a project');

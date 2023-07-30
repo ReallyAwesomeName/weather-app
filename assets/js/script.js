0602a3d13fe27d12d6a0f52f801efa861fa06210
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-07-30 18:47:41 -0400

    temp conversion

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,8 @@ var $searchesRecent = $("#searchesRecent").hide();
 $("#searchBtn").click(function (e) {
   e.preventDefault();
   console.log("clicked");
-  // const userQuery = $("#userQuery").val();
+  // const userQuery = $("input[name=userQuery]").val();
+  // console.log(userQuery);
   // const coords = getCoordinates(userQuery);
   // FIXME: get coords from user query
   testCoords = {
@@ -40,7 +41,6 @@ $("#searchBtn").click(function (e) {
       icon: JSON.stringify(data.list[0].weather[0].icon),
     };
     // ~~~~~~~~ DISPLAY RESULTS ~~~~~~~~
-    // TODO: make this pretty - this is not putting it in list
     const weatherCards = buildCards(data);
     $("#userPrompt").after(
       `<div class="row mx-5 my-4" id="weatherCardRow"></div>`
@@ -48,12 +48,6 @@ $("#searchBtn").click(function (e) {
     let theseCards = "";
     for ([key, value] of Object.entries(weatherCards)) {
       theseCards += value;
-      // $("#userPrompt").after(`${value}`);
-      // $weatherToday
-      //   .text(`${weather.temp}°C\n${weather.wind}mph\n${weather.humidity}%`)
-      //   .show();
-      // $weatherFiveDay.text(`TODO:PLACEHOLDER`).show();
-      // $searchesRecent.text(`TODO:PLACEHOLDER`).show();
     }
     $(`#weatherCardRow`).html(theseCards);
   });
@@ -61,69 +55,129 @@ $("#searchBtn").click(function (e) {
 
 // ~~~~~~~~ BUILD CARDS ~~~~~~~~
 function buildCards(weatherData) {
-  // FIXME: different times of same day instead of different days
   const weatherCards = {};
-  for (let i = 0; i < 5; i++) {
-    weatherCards[`weather_${i}`] = `
+  const nowDt = dayjs().add(1, "day").startOf("day").unix();
+  const endDt = dayjs().add(6, "day").startOf("day").unix();
+  for (let i = 0; i < weatherData.list.length; i++) {
+    var thisItem = weatherData.list[i];
+    if (!prevItem) {
+      if (weatherData.list[i - 1]) {
+        var prevItem = weatherData.list[i - 1];
+      }
+    }
+    if (prevItem) {
+      // breakpoint is for when one day has passed
+      let breakpoint = prevItem.dt;
+      let thisTime = thisItem.dt;
+      if (thisTime >= breakpoint) {
+        // adding card, update prevItem to thisItem
+        prevItem = thisItem;
+        if (thisItem.dt_txt.slice(11, 13) === "12") {
+          const thisDate = dayjs(thisItem.dt_txt).format("dddd, MMMM D");
+
+          weatherCards[`weather_${i}`] = `
     <div id="weather_${i}" class="card col-4 mx-auto">
-      <h5 class="card-header">${dayjs.unix(weatherData.list[i].dt)}</h5>
+      <h5 class="card-header">${thisDate}</h5>
       <div class="card-body">
         <ul class="list-group">
-          <li class="list-group-item">Temperature: ${
-            weatherData.list[i].main.temp
-          }°K</li>
+          <li class="list-group-item">Temperature: ${(
+            weatherData.list[i].main.temp - 273.15
+          ).toFixed(2)}°C</li>
           <li class="list-group-item">Condition: ${
             weatherData.list[i].weather[0].main
           }</li>
           <li class="list-group-item">Wind: ${
             weatherData.list[i].wind.speed
-          }</li>
+          } m/s</li>
           <li class="list-group-item">Humidity: ${
             weatherData.list[i].main.humidity
           }%</li>
-          <li class="list-group-item">${
+          <li class="list-group-item"><img src='https://openweathermap.org/img/w/${
             weatherData.list[i].weather[0].icon
-          }</li>
+          }.png'></li>
         </ul>
       </div>
     </div>
     `;
+        }
+      }
+    }
   }
   return weatherCards;
 }
 
 // ~~~~~~~~ API CALLS ~~~~~~~~
 // get coordinates from user input - either zip code or city name
-function getCoordinates(cityNameOrZip) {
-  return new Promise(function (resolve, reject) {
-    var zipcheck = /\d/g;
-    if (zipcheck.test(cityNameOrZip)) {
-      $.ajax({
-        url: `http://api.openweathermap.org/geo/1.0/zip?appid=${openWeatherApiKey}&zip=${cityNameOrZip}`,
-        method: "GET",
-        dataType: "json",
-        success: function (data) {
-          resolve(data);
-        },
-        error: function (err) {
-          reject(err);
-        },
-      });
-    } else {
-      $.ajax({
-        url: `http://api.openweathermap.org/geo/1.0/direct?q=${cityNameOrZip}&limit=1&appid=${openWeatherApiKey}`,
-        method: "GET",
-        dataType: "json",
-        success: function (data) {
-          resolve(data);
-        },
-        error: function (err) {
-          reject(err);
-        },
-      });
-    }
-  });
-}
+// function getCoordinates(userQuery) {
+//   const zipCheck = /^\d{5}$/;
+//   if (zipCheck.test(userQuery)) {
+//     fetch(
+//       `http://api.openweathermap.org/geo/1.0/zip?appid=${openWeatherApiKey}&zip=${userQuery}`
+//     )
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         const coords = { lat: data.lat, lon: data.lon };
+//         return coords;
+//       });
+//   } else {
+//     fetch(
+//       `http://api.openweathermap.org/geo/1.0/direct?q=${userQuery}&limit=5&appid=${openWeatherApiKey}`
+//     ).then(function (response) {
+//       return response.json();
+//     });
+//   }
+// }
+
+// FIXME: COORDINATES UNDEFINED
+// function getWeatherData(userQuery) {
+//   // get coordinates from user input - either zip code or city name
+//   const zipCheck = /^\d{5}$/;
+//   if (zipCheck.test(userQuery)) {
+//     fetch(
+//       `http://api.openweathermap.org/geo/1.0/zip?appid=${openWeatherApiKey}&zip=${userQuery}`
+//     )
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         const coordinates = { lat: data.lat, lon: data.lon };
+
+//         // use those coordinates to get weather data
+//         fetch(
+//           `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&units=imperial&appid=${openWeatherApiKey}`
+//         )
+//           .then(function (response) {
+//             return response.json();
+//           })
+//           .then(function (data) {
+//             return data;
+//           });
+//       });
+//   } else {
+//     fetch(
+//       `http://api.openweathermap.org/geo/1.0/direct?q=${userQuery}&limit=5&appid=${openWeatherApiKey}`
+//     )
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         const coordinates = { lat: data.lat, lon: data.lon };
+
+//         // use those coordinates to get weather data
+//         fetch(
+//           `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&units=imperial&appid=${openWeatherApiKey}`
+//         )
+//           .then(function (response) {
+//             return response.json();
+//           })
+//           .then(function (data) {
+//             return data;
+//           });
+//       });
+//   }
+// }
 
 // use coordinates to get weather data
 function getWeatherData(coordinates) {
@@ -141,16 +195,3 @@ function getWeatherData(coordinates) {
     });
   });
 }
-
-// ~~~~~~~~ DISPLAY RESULTS ~~~~~~~~
-// $weatherToday.text("wow");
-
-// testCoords = {
-//   lat: 40.7127,
-//   lon: -73.9872,
-// };
-
-// getWeatherData(testCoords).then(function (data) {
-//   const result = JSON.stringify(data.list[0].main.temp);
-//   $test.text(result);
-// });
